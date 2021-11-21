8c41c19d544dc84938dfd7ea16c160ffe8b269c4
Pcdoze <62628354+Pcdoze@users.noreply.github.com>
2021-11-21 15:26:31 -0300

    Update index.php

--- Questão_4/index.php
+++ Questão_4/index.php
@@ -1,9 +1,19 @@
-<!--
-Crie uma página utilizando HTML que receba o nome do usuário, uma senha e utilize a opção radio do elemento para a seleção do gênero (masculino e feminino). Envie para um programa PHP que mostre estas informações.
+<html>
+	<head>	
+		<title> MEDIA </title>
+	</head>
+	<body>
+		<form action="request.php" method="POST">
+			Nome: <input type="text" name="nome" id="nome"><br>
+			Senha: <input type="text" name="senha" id="senha"><br>
 
-Utilize o método adequado.
-
-
-
-Obs.: os códigos devem ser anexado a sua respectiva questão e o código copiado e colado no corpo da resposta abaixo.
-!-->
+			Gênero:<br>
+			<input type="radio" id="masc" name="genero" value="Masculino">
+  			<label for="masc">Masculino</label><br>
+  			<input type="radio" id="femi" name="genero" value="Feminino">
+  			<label for="femi">Feminino</label><br>
+		
+			<input type="submit" value="enviar"/>
+		</form>
+	</body>
+</html>
