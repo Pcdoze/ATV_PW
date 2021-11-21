80a9f7453ec990755a151df1e2c0ae5f93dcbc28
Pcdoze <62628354+Pcdoze@users.noreply.github.com>
2021-11-21 15:26:50 -0300

    Create request.php

---
+++ Questão_4/request.php
@@ -0,0 +1,10 @@
+<?php
+    $nome = $_POST['nome'];
+    $senha = $_POST['senha'];
+    $genero = $_POST['genero'];
+
+
+    echo "Nome: " . $nome;
+    echo "<br>Senha: " . $senha;
+    echo "<br>Gênero: " . $genero;
+?>
