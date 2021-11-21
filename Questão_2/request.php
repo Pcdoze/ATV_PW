c2d59b256c034861b09b9816feb66e2e0601d996
Pcdoze <62628354+Pcdoze@users.noreply.github.com>
2021-11-21 15:13:57 -0300

    Update request.php

--- Questão_2/request.php
+++ Questão_2/request.php
@@ -2,7 +2,7 @@
     $fixo = $_POST['fixo'];
     $vendas = $_POST['vendas'];
 
-    $total = vendas*fixo*0.04;
+    $total = $vendas*$fixo*0.04;
 
     echo "Salário Fixo: " . $fixo;
     echo "<br>Salário + Vendas: $total";
