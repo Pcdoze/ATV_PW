572280f23af93e610da70365cc879db03df7eb2d
Pcdoze <62628354+Pcdoze@users.noreply.github.com>
2021-11-21 15:14:21 -0300

    Update index.php

--- Questão_2/index.php
+++ Questão_2/index.php
@@ -1,11 +1,12 @@
-<!--
-    Um funcionário recebe um salário fixo mais 4% de comissão sobre as vendas. Faça uma aplicação Web, seguindo os seguintes requisitos:
-
- •Faça um documento HTML que permita ao usuário digitar o salário fixo de um funcionário e o valor total apurado em vendas;
-
- •Faça um script PHP que receba os dados submetidos pelo documento HTML anterior (método GET). O script deverá calcular e mostrar a comissão e o salário final do funcionário”;
-
-
-
-Obs.: os códigos devem ser anexado a sua respectiva questão e o código copiado e colado no corpo da resposta abaixo.
-!-->
+<html>
+	<head>	
+		<title> MEDIA </title>
+	</head>
+	<body>
+		<form action="request.php" method="POST">
+			Salário Fixo: <input type="text" name="fixo" id="fixo"><br>
+			Número de Vendas: <input type="text" name="vendas" id="vendas"><br>			
+			<input type="submit" value="enviar"/>
+		</form>
+	</body>
+</html>
