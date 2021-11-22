6578abebecd8bef819cfeb0ce998ccb75e029fb4
Pcdoze <62628354+Pcdoze@users.noreply.github.com>
2021-11-22 20:13:50 -0300

    Update index.php

--- Questão_2/index.php
+++ Questão_2/index.php
@@ -1,12 +1,31 @@
-<html>
-	<head>	
-		<title> MEDIA </title>
-	</head>
-	<body>
-		<form action="request.php" method="POST">
-			Salário Fixo: <input type="text" name="fixo" id="fixo"><br>
-			Número de Vendas: <input type="text" name="vendas" id="vendas"><br>			
-			<input type="submit" value="enviar"/>
-		</form>
-	</body>
+<!--
+Equipe:
+-->
+
+<?php
+if($_POST) {
+
+    $salario = $_POST['salario'];
+    $vendas = $_POST['vendas'];
+    $pctm = 0.04;
+    $resultado = $salario + ($vendas * $pctm);
+    echo "Seu salario devido a comissão é de: "; 
+    echo $resultado;
+    echo "<br>Sua comissão foi de:";
+    echo ($vendas * $pctm);
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+</html>
+    <head>
+        <title> Salario </title>
+    </head>
+    <body>
+    <form action="" method="POST">
+          <p>Informe seu salario fixo: <input type="text" name="salario" id="salario"/> </p>
+          <p>Informe o valor apurado em vendas: <input type="text" name="vendas" id="vendas"/> </p>
+            <input type="submit" value="Enviar"/>
+        </form>
+    </body>
 </html>
