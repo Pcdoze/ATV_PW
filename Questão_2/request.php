2311e91baa35e88dd6a8df5674cfb678636a647e
Pcdoze <62628354+Pcdoze@users.noreply.github.com>
2021-11-22 20:14:07 -0300

    Delete request.php

--- Questão_2/request.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $fixo = $_POST['fixo'];
-    $vendas = $_POST['vendas'];
-
-    $total = $vendas*$fixo*0.04;
-
-    echo "Salário Fixo: " . $fixo;
-    echo "<br>Salário + Vendas: $total";
-?>
